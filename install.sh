96d2ae39132b471a9cc9681015aeafe12bb55207
Ai Makabi <a.macbee@gmail.com>
2015-12-07 21:03:10 +0900

    とりあえずコケるやつはインストールしない感じになってる

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 apt-get update
 apt-get install -y unzip ghex git
-git clone https://github.com/radare/radare2
-cd radare2
-sys/install.sh
+
+# これがコケるんだけどよく分からず
+# git clone https://github.com/radare/radare2
+# cd radare2
+# sys/install.sh
